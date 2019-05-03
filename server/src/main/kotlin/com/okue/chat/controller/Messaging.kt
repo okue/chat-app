49f66ee354400a8faaf52f3b478eb5e0e877e8e2
okue <nogideca@gmail.com>
2019-05-03 16:36:48 +0900

    implement grpc methods, but empty

--- server/src/main/kotlin/com/okue/chat/controller/Messaging.kt
+++ server/src/main/kotlin/com/okue/chat/controller/Messaging.kt
@@ -1,9 +1,13 @@
 package com.okue.chat.controller
 
 import com.okue.controller.MessagingGrpc
+import com.okue.controller.Msg
+import com.okue.controller.ReceiveMsgsReply
+import com.okue.controller.ReceiveMsgsRequest
 import com.okue.controller.Reply
 import com.okue.controller.SendMsgReply
 import com.okue.controller.SendMsgRequest
+import com.okue.controller.User
 import io.grpc.stub.StreamObserver
 import org.lognet.springboot.grpc.GRpcService
 
@@ -11,11 +15,10 @@ import org.lognet.springboot.grpc.GRpcService
 // https://github.com/LogNet/grpc-spring-boot-starter
 
 // NOTE for grpcc
-// client.sendMsg({user: {name: "hoge"}, msg: {from: { name: "hoge" }, to: { name : "fuga" }, content: "aaaaa"}}, pr)
+// client.receiveMsgs({user : { name : "fuga" }}).on('data', sr).on('status', sr)
 
 @GRpcService
 class Messaging : MessagingGrpc.MessagingImplBase() {
-
     override fun sendMsg(request: SendMsgRequest, responseObserver: StreamObserver<SendMsgReply>) {
         val msg = request.msg
         println("from ${msg.from}, to ${msg.to}, content ${msg.content}")
@@ -29,6 +32,23 @@ class Messaging : MessagingGrpc.MessagingImplBase() {
             .setReply(reply)
             .build()
         responseObserver.onNext(reply2)
+        responseObserver.onCompleted()
+    }
+
+    override fun receiveMsgs(request: ReceiveMsgsRequest, responseObserver: StreamObserver<ReceiveMsgsReply>) {
+        val user = request.user
+        println("user ${user.name}")
+        val msg = Msg
+            .newBuilder()
+            .setFrom(User.newBuilder().setName("sakana"))
+            .setTo(User.newBuilder().setName("misoyaki"))
+        val reply = ReceiveMsgsReply
+            .newBuilder()
+            .setMsg(msg)
+            .build()
+        repeat(3) {
+            responseObserver.onNext(reply)
+        }
         responseObserver.onCompleted()
     }
 }
